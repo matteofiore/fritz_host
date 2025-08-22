75faaa7d526c4ce7bf7a27f435c1cca7e41565dc
matteofiore <82138893+matteofiore@users.noreply.github.com>
2025-08-23 00:22:12 +0200

    Update config_flow.py

--- custom_components/fritz_hosts/config_flow.py
+++ custom_components/fritz_hosts/config_flow.py
@@ -1,24 +1,21 @@
-import voluptuous as vol
+"""Config flow for Fritz!Box Host Sensor integration."""
 from homeassistant import config_entries
+import voluptuous as vol
 
-DOMAIN = "fritz_hosts"
+from .const import DOMAIN
 
-class FritzHostsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Fritz!Box Host Sensor."""
-
-    VERSION = 1
+class FritzHostsFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for FritzHosts."""
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
-        errors = {}
         if user_input is not None:
-            # Qui puoi validare host, username e password
             return self.async_create_entry(title="Fritz!Box", data=user_input)
 
         data_schema = vol.Schema({
-            vol.Required("host", default="192.168.1.1"): str,
-            vol.Required("username"): str,
+            vol.Required("address", default="192.168.1.1"): str,
+            vol.Required("user"): str,
             vol.Required("password"): str,
         })
 
-        return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)
+        return self.async_show_form(step_id="user", data_schema=data_schema)
