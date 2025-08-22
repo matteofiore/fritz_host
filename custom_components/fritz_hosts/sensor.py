732782d810b60793a22f12cd322c3baa261f0b46
matteofiore <82138893+matteofiore@users.noreply.github.com>
2025-08-23 00:10:02 +0200

    Update sensor.py

--- custom_components/fritz_hosts/sensor.py
+++ custom_components/fritz_hosts/sensor.py
@@ -1,27 +1,22 @@
-import logging
 from homeassistant.components.sensor import SensorEntity
-from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD
+from .const import DOMAIN
 from fritzconnection import FritzConnection
 from fritzconnection.lib.fritzhosts import FritzHosts
 
-_LOGGER = logging.getLogger(__name__)
-
 async def async_setup_entry(hass, entry, async_add_entities):
-    """Set up sensor from config flow entry."""
-    ip = entry.data[CONF_HOST]
-    user = entry.data[CONF_USERNAME]
-    password = entry.data[CONF_PASSWORD]
-    async_add_entities([FritzHostsSensor(ip, user, password)])
+    host = entry.data["host"]
+    user = entry.data["username"]
+    password = entry.data["password"]
 
-class FritzHostsSensor(SensorEntity):
-    """Sensor che mostra il numero di dispositivi FritzBox attivi."""
+    fc = FritzConnection(address=host, user=user, password=password)
+    hosts = FritzHosts(fc)
+    async_add_entities([FritzActiveHostsSensor(hosts)])
 
-    def __init__(self, ip, user, password):
+class FritzActiveHostsSensor(SensorEntity):
+    def __init__(self, hosts):
+        self._hosts = hosts
         self._state = None
-        self._name = "FritzBox Active Hosts"
-        self._ip = ip
-        self._user = user
-        self._password = password
+        self._name = "Fritz Active Hosts"
 
     @property
     def name(self):
@@ -31,16 +26,5 @@ class FritzHostsSensor(SensorEntity):
     def state(self):
         return self._state
 
-    @property
-    def unit_of_measurement(self):
-        return "dispositivi"
-
     async def async_update(self):
-        try:
-            fc = FritzConnection(address=self._ip, user=self._user, password=self._password)
-            hosts = FritzHosts(fc)
-            active_hosts = hosts.get_active_hosts()
-            self._state = len(active_hosts)
-        except Exception as e:
-            _LOGGER.error(f"Errore aggiornamento FritzHostsSensor: {e}")
-            self._state = None
+        self._state = len(self._hosts.get_active_hosts())
