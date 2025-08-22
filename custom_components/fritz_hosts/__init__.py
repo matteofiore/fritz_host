e21aff383cc44740d0a113a199612b45f4410705
matteofiore <82138893+matteofiore@users.noreply.github.com>
2025-08-23 00:21:49 +0200

    Update __init__.py

--- custom_components/fritz_hosts/__init__.py
+++ custom_components/fritz_hosts/__init__.py
@@ -1,1 +1,21 @@
+"""Fritz!Box Host Sensor integration."""
 
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+
+DOMAIN = "fritz_hosts"
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the integration from configuration.yaml (optional)."""
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up the integration from a config flow entry."""
+    # Qui puoi salvare l'IP, utente e password
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    # Creazione del sensore
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
