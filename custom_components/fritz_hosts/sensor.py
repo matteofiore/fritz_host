b4914e47ac6dbb1ad670eec7e1d042c93e848a6e
matteofiore <82138893+matteofiore@users.noreply.github.com>
2025-08-23 00:22:01 +0200

    Update sensor.py

--- custom_components/fritz_hosts/sensor.py
+++ custom_components/fritz_hosts/sensor.py
@@ -1,54 +1,30 @@
-from datetime import timedelta
-import logging
+"""Fritz!Box host sensor."""
 
 from homeassistant.components.sensor import SensorEntity
-from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD
-from homeassistant.helpers.update_coordinator import (
-    DataUpdateCoordinator,
-    CoordinatorEntity,
-)
+from .const import DOMAIN
 from fritzconnection import FritzConnection
 from fritzconnection.lib.fritzhosts import FritzHosts
 
-_LOGGER = logging.getLogger(__name__)
-
-DOMAIN = "fritz_hosts"
-
-async def async_setup_entry(hass, config_entry, async_add_entities):
-    """Set up the FritzHosts sensor with periodic updates."""
-    host = config_entry.data[CONF_HOST]
-    username = config_entry.data[CONF_USERNAME]
-    password = config_entry.data[CONF_PASSWORD]
-
-    fc = FritzConnection(address=host, user=username, password=password)
-    hosts = FritzHosts(fc)
-
-    async def async_update_data():
-        """Fetch active hosts from FritzBox."""
-        return len(hosts.get_active_hosts())
-
-    coordinator = DataUpdateCoordinator(
-        hass,
-        _LOGGER,
-        name="fritz_hosts",
-        update_method=async_update_data,
-        update_interval=timedelta(minutes=1),  # aggiorna ogni minuto
+async def async_setup_entry(hass, entry, async_add_entities):
+    """Set up sensor from config flow entry."""
+    config = entry.data
+    fc = FritzConnection(
+        address=config["address"],
+        user=config["user"],
+        password=config["password"]
     )
+    hosts = FritzHosts(fc)
+    active_hosts = hosts.get_active_hosts()
+    
+    async_add_entities([FritzHostsSensor(len(active_hosts))])
 
-    # primo aggiornamento
-    await coordinator.async_config_entry_first_refresh()
+class FritzHostsSensor(SensorEntity):
+    """Representation of a Fritz!Box hosts sensor."""
 
-    async_add_entities([FritzHostsCoordinatorSensor(coordinator)])
-
-
-class FritzHostsCoordinatorSensor(CoordinatorEntity, SensorEntity):
-    """FritzHosts sensor using DataUpdateCoordinator."""
-
-    def __init__(self, coordinator):
-        super().__init__(coordinator)
+    def __init__(self, count):
         self._attr_name = "Fritz!Box Active Hosts"
-        self._attr_unit_of_measurement = "devices"
+        self._attr_native_value = count
 
     @property
     def native_value(self):
-        return self.coordinator.data
+        return self._attr_native_value
